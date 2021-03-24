648d8d2961149ce7d0022a192482f32ef03a09ee
Javan Torres <javantorres@gmail.com>
2021-03-24 00:43:07 -0300

    .

---
+++ Curso-S9-ProjetoFinal/email-worker-compose/scripts/check.sql
@@ -0,0 +1,6 @@
+/*Listar todos os bacos de dados cadastrados*/
+\l
+/*Se conectar ao  BD email_sender*/
+\c email_sender
+/*Gerar um descrição da tabela emails*/
+\d emails
---
+++ Curso-S9-ProjetoFinal/email-worker-compose/scripts/ini.sql
@@ -0,0 +1,10 @@
+create database email_sender;
+/*Comando para conectar na base de dados*/
+\c email_sender
+
+create table emails (
+    id serial not null,
+    data timestamp not null default current_timestamp,
+    assunto varchar(100) not null,
+    mensagem varchar(250) not null
+);
